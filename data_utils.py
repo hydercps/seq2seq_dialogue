b9912621d2b7e92f0811cbacc912ab7acb2e4e6f
Igor Shalyminov <ishalyminov@gmail.com>
2016-11-06 21:04:38 +0000

    initial commit

---
+++ data_utils.py
@@ -0,0 +1,257 @@
+"""Utilities for downloading data from WMT, tokenizing, vocabularies."""
+
+from os import path
+import re
+import logging
+
+import numpy as np
+
+from keras.preprocessing.sequence import pad_sequences
+from gensim.models import Word2Vec
+
+logger = logging.getLogger()
+
+# Special vocabulary symbols - we always put them at the start.
+PAD = '__PAD__'
+GO = '__GO__'
+STOP = '__STOP__'
+UNK = '__UNK__'
+START_VOCAB = [PAD, GO, STOP, UNK]
+
+PAD_ID = 0
+GO_ID = 1
+STOP_ID = 2
+UNK_ID = 3
+
+# Regular expressions used to tokenize.
+WORD_SPLIT = re.compile(b"([.,!?\"':;)(])")
+DIGIT_RE = re.compile(br"\d")
+
+
+def basic_tokenizer(sentence):
+    """Very basic tokenizer: split the sentence into a list of tokens."""
+    words = []
+    for space_separated_fragment in sentence.strip().split():
+        words.extend(re.split(WORD_SPLIT, space_separated_fragment))
+    return [w for w in words if w]
+
+
+def create_vocabulary(
+    vocabulary_path,
+    data_path,
+    max_vocabulary_size,
+    tokenizer=None,
+    normalize_digits=True
+):
+    if not path.exists(vocabulary_path):
+        print("Creating vocabulary %s from %s" % (vocabulary_path, data_path))
+        vocab = {}
+        with open(data_path, mode="rb") as f:
+            counter = 0
+            for line in f:
+                counter += 1
+                if counter % 100000 == 0:
+                    print("  processing line %d" % counter)
+                tokens = tokenizer(line) if tokenizer else basic_tokenizer(line)
+                for w in tokens:
+                    word = re.sub(DIGIT_RE, b"0", w) if normalize_digits else w
+                    if word in vocab:
+                        vocab[word] += 1
+                    else:
+                        vocab[word] = 1
+            vocab_list = START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+            print('>> Full Vocabulary Size :',len(vocab_list))
+            if len(vocab_list) > max_vocabulary_size:
+                vocab_list = vocab_list[:max_vocabulary_size]
+            with open(vocabulary_path, mode="wb") as vocab_file:
+                for w in vocab_list:
+                    vocab_file.write(w + b"\n")
+
+
+def initialize_vocabulary(vocabulary_path):
+    if path.exists(vocabulary_path):
+        rev_vocab = []
+        with open(vocabulary_path, mode="rb") as f:
+            rev_vocab.extend(f.readlines())
+        rev_vocab = [line.strip() for line in rev_vocab]
+        vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
+        return vocab, rev_vocab
+    else:
+        raise ValueError("Vocabulary file %s not found.", vocabulary_path)
+
+
+def sentence_to_token_ids(sentence, vocabulary, tokenizer=None, normalize_digits=True):
+    if tokenizer:
+        words = tokenizer(sentence)
+    else:
+        words = basic_tokenizer(sentence)
+    if not normalize_digits:
+        return [vocabulary.get(w, UNK_ID) for w in words]
+    # Normalize digits by 0 before looking words up in the vocabulary.
+    return [vocabulary.get(re.sub(DIGIT_RE, b"0", w), UNK_ID) for w in words]
+
+
+def data_to_token_ids(
+    data_path,
+    target_path,
+    vocabulary_path,
+    tokenizer=None,
+    normalize_digits=True
+):
+    if not path.exists(target_path):
+        print("Tokenizing data in %s" % data_path)
+        vocab, _ = initialize_vocabulary(vocabulary_path)
+        with open(data_path, mode="rb") as data_file, open(target_path, mode="w") as tokens_file:
+            counter = 0
+            for line in data_file:
+                counter += 1
+                if counter % 100000 == 0:
+                    print("  tokenizing line %d" % counter)
+                token_ids = sentence_to_token_ids(
+                    line,
+                    vocab,
+                    tokenizer,
+                    normalize_digits
+                )
+                tokens_file.write(" ".join([str(tok) for tok in token_ids]) + "\n")
+
+
+def prepare_custom_data(
+    working_directory,
+    train_enc,
+    train_dec,
+    test_enc,
+    test_dec,
+    w2v_model_path,
+    embeddings_path,
+    in_buckets,
+    tokenizer=None
+):
+    max_vocabulary_size = 100000
+    # Create vocabularies of the appropriate sizes.
+    enc_vocab_path = path.join(working_directory, "vocab.enc")
+    dec_vocab_path = path.join(working_directory, "vocab.dec")
+    create_vocabulary(enc_vocab_path, train_enc, max_vocabulary_size, tokenizer)
+    create_vocabulary(dec_vocab_path, train_dec, max_vocabulary_size, tokenizer)
+
+    # only building embeddings for the encoder vocabulary
+    enc_vocab, enc_rev_vocab = initialize_vocabulary(enc_vocab_path)
+    dec_vocab, dec_rev_vocab = initialize_vocabulary(dec_vocab_path)
+    
+    if not path.exists(embeddings_path):
+        logger.info('Building embeddings matrix for encoder vocabulary')
+        embeddings = build_embedding_matrix(
+            enc_vocab,
+            Word2Vec.load_word2vec_format(w2v_model_path, binary=True)
+        )
+        with open(embeddings_path, 'wb') as embeddings_out:
+            np.save(embeddings_out, embeddings)
+    else:
+        embeddings = np.load(embeddings_path)
+
+    # Create token ids for the training data.
+    enc_train_ids_path = train_enc + (".ids")
+    dec_train_ids_path = train_dec + (".ids")
+    data_to_token_ids(train_enc, enc_train_ids_path, enc_vocab_path, tokenizer)
+    data_to_token_ids(train_dec, dec_train_ids_path, dec_vocab_path, tokenizer)
+
+    # Create token ids for the development data.
+    enc_dev_ids_path = test_enc + (".ids")
+    dec_dev_ids_path = test_dec + (".ids")
+    data_to_token_ids(test_enc, enc_dev_ids_path, enc_vocab_path, tokenizer)
+    data_to_token_ids(test_dec, dec_dev_ids_path, dec_vocab_path, tokenizer)
+
+    train_set = pad_and_bucket(enc_train_ids_path, dec_train_ids_path, in_buckets)
+    dev_set = pad_and_bucket(enc_dev_ids_path, dec_dev_ids_path, in_buckets)
+    for bucket in train_set:
+        train_set[bucket]['inputs'] = np.asarray(train_set[bucket]['inputs'], dtype=np.int32)
+        train_set[bucket]['outputs'] = ids_to_one_hots(train_set[bucket]['outputs'], len(dec_vocab))
+    for bucket in dev_set:
+        dev_set[bucket]['inputs'] = np.asarray(dev_set[bucket]['inputs'], dtype=np.int32)
+        dev_set[bucket]['outputs'] = ids_to_one_hots(dev_set[bucket]['outputs'], len(dec_vocab))
+    return enc_vocab, enc_rev_vocab, dec_vocab, dec_rev_vocab, embeddings, train_set, dev_set
+
+
+def pad_and_bucket(source_path, target_path, buckets, max_size=None):
+    """Read data from source and target files and put into buckets.
+
+    Args:
+        source_path: path to the files with token-ids for the source language.
+        target_path: path to the file with token-ids for the target language;
+            it must be aligned with the source file: n-th line contains the desired
+            output for n-th line from the source_path.
+        max_size: maximum number of lines to read, all other will be ignored;
+            if 0 or None, data files will be read completely (no limit).
+
+    Returns:
+        data_set: a list of length len(_buckets); data_set[n] contains a list of
+            (source, target) pairs read from the provided data files that fit
+            into the n-th bucket, i.e., such that len(source) < _buckets[n][0] and
+            len(target) < _buckets[n][1]; source and target are lists of token-ids.
+    """
+    # data_set = [[] for _ in buckets]
+    data_set = {
+        bucket: {
+            'inputs': [],
+            'outputs': []
+        }
+        for bucket in buckets
+    }
+
+    with open(source_path) as source_file, open(target_path) as target_file:
+        source, target = source_file.readline(), target_file.readline()
+        counter = 0
+        while source and target and (not max_size or counter < max_size):
+            counter += 1
+            source_ids = [int(x) for x in source.split()[:-1]]
+            target_ids = [GO_ID] + [int(x) for x in target.split()] + [STOP_ID]
+
+            for source_size, target_size in data_set:
+                if len(source_ids) < source_size and len(target_ids) < target_size:
+                    source_ids_padded = pad_sequences(
+                        [source_ids],
+                        maxlen=source_size,
+                        padding='pre',
+                        dtype='int32',
+                        value=PAD_ID
+                    )[0]
+                    target_ids_padded = pad_sequences(
+                        [target_ids],
+                        maxlen=target_size,
+                        padding='post',
+                        dtype='int32',
+                        value=PAD_ID
+                    )[0]
+                    data_set[(source_size, target_size)]['inputs'].append(
+                        source_ids_padded
+                    )
+                    data_set[(source_size, target_size)]['outputs'].append(
+                        target_ids_padded
+                    )
+                    break
+            source, target = source_file.readline(), target_file.readline()
+    return data_set
+
+
+def ids_to_one_hots(in_ids_bucket, in_vocabulary_size):
+    if not len(in_ids_bucket):
+        return None
+    result = np.zeros(
+        (len(in_ids_bucket), len(in_ids_bucket[0]), in_vocabulary_size + 1), 
+        dtype=np.int32
+    )
+    for sequence_id, sequence in enumerate(in_ids_bucket):
+        for id_index, token_id in enumerate(sequence):
+            result[sequence_id][id_index][token_id] = 1
+    return result
+
+
+def build_embedding_matrix(in_vocabulary, in_w2v_model):
+    EMBEDDING_DIM = in_w2v_model.vector_size
+
+    result = np.zeros((len(in_vocabulary) + 1, EMBEDDING_DIM))
+    for word, word_index in in_vocabulary.iteritems():
+        if word in in_w2v_model:
+            # words not found in embedding index will be all-zeros.
+            result[word_index] = in_w2v_model[word]
+    return result
---
+++ seq2seq_dialogue.py
@@ -0,0 +1,281 @@
+from os import getcwd, path, makedirs
+import logging
+
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Embedding
+
+from seq2seq.models import AttentionSeq2Seq
+
+from data_utils import pad_and_bucket, prepare_custom_data, START_VOCAB
+
+logging.getLogger().setLevel('INFO')
+
+# We use a number of buckets and pad to the closest one for efficiency.
+# See seq2seq_model.Seq2SeqModel for details of how they work.
+BUCKETS = [(5, 10), (10, 15), (20, 25), (40, 50)]
+
+VOCABULARY_SIZE = 40000 + len(START_VOCAB)
+LAYER_SIZE = 256
+MAX_LAYERS = 3
+MAX_GRADIENT_NORM = 5.0
+BATCH_SIZE = 1
+LEARNING_RATE = 9.0
+LEARNING_RATE_DECAY_FACTOR = 0.01
+FORWARD_ONLY = False
+
+DATASET_DIR = path.join(
+    getcwd(),
+    '..',
+    'opensubtitles_tools',
+    'opensubtitles_seq2seq_dataset'
+)
+WORKING_DIR = path.join(getcwd(), 'result')
+WORD2VEC_MODEL_PATH = path.join(
+    getcwd(),
+    '..',
+    'word2vec_google_news',
+    'GoogleNews-vectors-negative300.bin'
+)
+
+
+def create_model(in_encoder_vocabulary, in_decoder_vocabulary, in_embedding_matrix):
+    effective_vocabulary_size, embedding_size = in_embedding_matrix.shape
+    embedding_layer = Embedding(
+        len(in_encoder_vocabulary) + 1,
+        embedding_size,
+        weights=[in_embedding_matrix],
+        input_length=BUCKETS[1][0],
+        trainable=True,
+        name='emb'
+    )
+    seq2seq_model = AttentionSeq2Seq(
+        bidirectional=False,
+        input_dim=embedding_size,
+        input_length=BUCKETS[1][0],
+        output_dim=len(in_decoder_vocabulary) + 1,
+        hidden_dim=64,
+        output_length=BUCKETS[1][1],
+        depth=3,
+        dropout=0.2
+    )
+    model = Sequential()
+    model.add(embedding_layer)
+    model.add(seq2seq_model)
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
+
+def prepare_data():
+    # prepare dataset
+    logging.info('Preparing data')
+    if not path.exists(WORKING_DIR):
+        makedirs(WORKING_DIR)
+    return prepare_custom_data(
+        WORKING_DIR,
+        path.join(DATASET_DIR, 'train.enc'),
+        path.join(DATASET_DIR, 'train.dec'),
+        path.join(DATASET_DIR, 'test.enc'),
+        path.join(DATASET_DIR, 'test.dec'),
+        WORD2VEC_MODEL_PATH,
+        path.join(DATASET_DIR, 'embeddings.npy'),
+        BUCKETS
+    )
+
+
+def train(train_set, dev_set):
+    train_bucket_sizes = [len(train_set[b]) for b in xrange(len(BUCKETS))]
+    train_total_size = float(sum(train_bucket_sizes))
+
+    # A bucket scale is a list of increasing numbers from 0 to 1 that we'll use
+    # to select a bucket. Length of [scale[i], scale[i+1]] is proportional to
+    # the size if i-th training bucket, as used later.
+    train_buckets_scale = [
+        sum(train_bucket_sizes[:i + 1]) / train_total_size
+        for i in xrange(len(train_bucket_sizes))
+    ]
+
+    model = create_model()
+    # Choose a bucket according to data distribution. We pick a random number
+    # in [0, 1] and use the corresponding interval in train_buckets_scale.
+    random_number_01 = np.random.random_sample()
+    bucket_id = min([
+        i for i in xrange(len(train_buckets_scale))
+        if random_number_01 < train_buckets_scale[i]
+    ])
+    # Get a batch and make a step
+    encoder_inputs, decoder_inputs, target_weights = model.get_batch(train_set, bucket_id)
+    model.fit(encoder_inputs, decoder_inputs)
+    return model
+
+    with tf.Session(config=config) as sess:
+
+
+        # This is the training loop.
+        step_time, loss = 0.0, 0.0
+        current_step = 0
+        previous_losses = []
+        while True:
+
+          start_time = time.time()
+          _, step_loss, _ = model.step(sess, encoder_inputs, decoder_inputs,
+                                       target_weights, bucket_id, False)
+          step_time += (time.time() - start_time) / gConfig['steps_per_checkpoint']
+          loss += step_loss / gConfig['steps_per_checkpoint']
+          current_step += 1
+
+          # Once in a while, we save checkpoint, print statistics, and run evals.
+          if current_step % gConfig['steps_per_checkpoint'] == 0:
+            # Print statistics for the previous epoch.
+            perplexity = math.exp(loss) if loss < 300 else float('inf')
+            print ("global step %d learning rate %.4f step-time %.2f perplexity "
+                   "%.2f" % (model.global_step.eval(), model.learning_rate.eval(),
+                             step_time, perplexity))
+            # Decrease learning rate if no improvement was seen over last 3 times.
+            if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
+              sess.run(model.learning_rate_decay_op)
+            previous_losses.append(loss)
+            # Save checkpoint and zero timer and loss.
+            checkpoint_path = path.join(gConfig['working_directory'], "seq2seq.ckpt")
+            model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+            step_time, loss = 0.0, 0.0
+            # Run evals on development set and print their perplexity.
+            for bucket_id in xrange(len(_buckets)):
+              if len(dev_set[bucket_id]) == 0:
+                print("  eval: empty bucket %d" % (bucket_id))
+                continue
+              encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+                  dev_set, bucket_id)
+              _, eval_loss, _ = model.step(sess, encoder_inputs, decoder_inputs,
+                                           target_weights, bucket_id, True)
+              eval_ppx = math.exp(eval_loss) if eval_loss < 300 else float('inf')
+              print("  eval: bucket %d perplexity %.2f" % (bucket_id, eval_ppx))
+            sys.stdout.flush()
+
+
+def decode():
+  with tf.Session() as sess:
+    # Create model and load parameters.
+    model = create_model(sess, True)
+    model.batch_size = 1  # We decode one sentence at a time.
+
+    # Load vocabularies.
+    enc_vocab_path = os.path.join(gConfig['working_directory'],"vocab%d.enc" % gConfig['enc_vocab_size'])
+    dec_vocab_path = os.path.join(gConfig['working_directory'],"vocab%d.dec" % gConfig['dec_vocab_size'])
+
+    enc_vocab, _ = data_utils.initialize_vocabulary(enc_vocab_path)
+    _, rev_dec_vocab = data_utils.initialize_vocabulary(dec_vocab_path)
+
+    # Decode from standard input.
+    sys.stdout.write("> ")
+    sys.stdout.flush()
+    sentence = sys.stdin.readline()
+    while sentence:
+      # Get token-ids for the input sentence.
+      token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), enc_vocab)
+      # Which bucket does it belong to?
+      bucket_id = min([b for b in xrange(len(_buckets))
+                       if _buckets[b][0] > len(token_ids)])
+      # Get a 1-element batch to feed the sentence to the model.
+      encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+          {bucket_id: [(token_ids, [])]}, bucket_id)
+      # Get output logits for the sentence.
+      _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
+                                       target_weights, bucket_id, True)
+      # This is a greedy decoder - outputs are just argmaxes of output_logits.
+      outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+      # If there is an EOS symbol in outputs, cut them at that point.
+      if data_utils.EOS_ID in outputs:
+        outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+      # Print out French sentence corresponding to outputs.
+      print " ".join([tf.compat.as_str(rev_dec_vocab[output]) for output in outputs])
+      print "> "
+      sys.stdout.flush()
+      sentence = sys.stdin.readline()
+
+
+def self_test():
+  """Test the translation model."""
+  with tf.Session() as sess:
+    print("Self-test for neural translation model.")
+    # Create model with vocabularies of 10, 2 small buckets, 2 layers of 32.
+    model = seq2seq_model.Seq2SeqModel(10, 10, [(3, 3), (6, 6)], 32, 2,
+                                       5.0, 32, 0.3, 0.99, num_samples=8)
+    sess.run(tf.initialize_all_variables())
+
+    # Fake data set for both the (3, 3) and (6, 6) bucket.
+    data_set = ([([1, 1], [2, 2]), ([3, 3], [4]), ([5], [6])],
+                [([1, 1, 1, 1, 1], [2, 2, 2, 2, 2]), ([3, 3, 3], [5, 6])])
+    for _ in xrange(5):  # Train the fake model for 5 steps.
+      bucket_id = random.choice([0, 1])
+      encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+          data_set, bucket_id)
+      model.step(sess, encoder_inputs, decoder_inputs, target_weights,
+                 bucket_id, False)
+
+
+def init_session(sess, conf='seq2seq.ini'):
+    global gConfig
+    gConfig = get_config(conf)
+ 
+    # Create model and load parameters.
+    model = create_model(sess, True)
+    model.batch_size = 1  # We decode one sentence at a time.
+
+    # Load vocabularies.
+    enc_vocab_path = os.path.join(gConfig['working_directory'],"vocab%d.enc" % gConfig['enc_vocab_size'])
+    dec_vocab_path = os.path.join(gConfig['working_directory'],"vocab%d.dec" % gConfig['dec_vocab_size'])
+
+    enc_vocab, _ = data_utils.initialize_vocabulary(enc_vocab_path)
+    _, rev_dec_vocab = data_utils.initialize_vocabulary(dec_vocab_path)
+
+    return sess, model, enc_vocab, rev_dec_vocab
+
+
+def decode_line(sess, model, enc_vocab, rev_dec_vocab, sentence):
+    # Get token-ids for the input sentence.
+    token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), enc_vocab)
+
+    # Which bucket does it belong to?
+    bucket_id = min([b for b in xrange(len(_buckets)) if len(token_ids) < BUCKETS[b][0]])
+
+    # Get a 1-element batch to feed the sentence to the model.
+    encoder_inputs, decoder_inputs, target_weights = model.get_batch({bucket_id: [(token_ids, [])]}, bucket_id)
+
+    # Get output logits for the sentence.
+    _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs, target_weights, bucket_id, True)
+
+    # This is a greedy decoder - outputs are just argmaxes of output_logits.
+    outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+
+    # If there is an EOS symbol in outputs, cut them at that point.
+    if data_utils.STOP_ID in outputs:
+        outputs = outputs[:outputs.index(data_utils.STOP_ID)]
+
+    return " ".join([tf.compat.as_str(rev_dec_vocab[output]) for output in outputs])
+
+
+def main():
+    enc_vocab, enc_rev_vocab, dec_vocab, dec_rev_vocab, embeddings, train_set, dev_set = prepare_data()
+    model = create_model(enc_vocab, dec_vocab, embeddings)
+    targets = np.zeros(
+        (len(train_set[BUCKETS[1]]['outputs']), BUCKETS[1][1], len(enc_vocab) + 1), 
+        dtype=np.int32
+    )
+    model.fit(
+        train_set[BUCKETS[1]]['inputs'],
+        train_set[BUCKETS[1]]['outputs'],
+        batch_size=1,
+        nb_epoch=10
+    )
+    model.evaluate(
+        dev_set[BUCKETS[1]]['inputs'],
+        dev_set[BUCKETS[1]]['outputs'] 
+    )
+    model_file = path.join(WORKING_DIR, 'model.h5')
+    model.save(model_file, overwrite=True)
+
+
+if __name__ == '__main__':
+    main()
